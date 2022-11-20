2a6bc083ba6181ef88e60793958180cbc6b7289d
soodax <mirakov.david@mail.ru>
2022-11-20 14:57:16 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { FormControl, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angularTest';
+export class AppComponent implements OnInit{
+
+  contactsForm = new FormGroup({
+    city: new FormControl('', Validators.required),
+    street: new FormControl('', Validators.required),
+    apartment: new FormControl('', Validators.required),
+    phoneArray: new FormArray([
+      new FormControl('', [Validators.required, Validators.minLength(10)])
+    ])
+  })
+
+  phoneArray() {
+    return this.contactsForm.get('phoneArray') as FormArray
+  }
+
+  addNewPhone() {
+    this.contactsForm.controls.phoneArray.push(new FormControl('', [Validators.required, Validators.minLength(10)]))
+  }
+
+  onSubmit() {
+    if (this.contactsForm.valid) {
+      return alert('pass')
+    }
+
+    alert('error')
+  }
+
+  ngOnInit() {
+  }
 }
